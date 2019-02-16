1c5a5ebbcf9d0317f2c16f31b2ca0336965a75ea
Dishant <500067499@stu.upes.ac.in>
2019-02-16 16:08:02 +0530

    removed redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,74 +1,84 @@
 class ArticlesController < ApplicationController
-	
-	def new
-	
-		@article = Article.new
-	
-	end
 
-	def index
-		@articles=Article.all
-	end
+before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-	def edit
-		@article = Article.find(params[:id])
-	end
+def index
 
-	def update
+	@articles = Article.all
 
-		@article = Article.find(params[:id])
+end
 
-		if @article.update(article_params)
+def new
 
-			flash[:notice] = "Article was successfully updated"
+	@article = Article.new
 
-			redirect_to @article
+end
 
-		else
+def edit
 
-			render 'edit'
-		end
-		
-	end
+end
 
-	def create
+def create
 
-		@article = Article.new(article_params)
+	@article = Article.new(article_params)
 
-		if @article.save
+	if @article.save
 
-			flash[:notice] = "Article was successfully created"
-	
-			redirect_to article_path(@article)
+		flash[:notice] = "Article was successfully created"
 
-		else
+		redirect_to article_path(@article)
 
-			render 'new'
+	else
 
-		end
-
-	end
-
-	def destroy
-		@article = Article.find(params[:id])
-
-		@article.destroy
-
-		flash[:notice] = "Article was successfully deleted"
-
-		redirect_to articles_path
-	end
-
-	def show
-		@article = Article.find(params[:id])
-	end
-
-	private
-
-	def article_params
-
-    	params.require(:article).permit(:title, :description)
+		render 'new'
 
 	end
 
 end
+
+def update
+
+	if @article.update(article_params)
+
+		flash[:notice] = "Article was successfully updated"
+
+		redirect_to article_path(@article)
+
+	else
+
+		render 'edit'
+
+	end
+
+end
+
+def show
+
+end
+
+def destroy
+
+	@article.destroy
+
+	flash[:notice] = "Article was successfully deleted"
+
+	redirect_to articles_path
+
+end
+
+private
+
+def set_article
+
+	@article = Article.find(params[:id])
+
+end
+
+def article_params
+
+	params.require(:article).permit(:title, :description)
+
+end
+
+end
+
